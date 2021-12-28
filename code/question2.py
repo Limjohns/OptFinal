1b86d7ece2b4f3e586c39a1bc15d1922c53667e2
Limjohns <420059421@qq.com>
2021-12-28 22:04:58 +0800

    Update question2.py

--- code/question2.py
+++ code/question2.py
@@ -24,6 +24,68 @@ import random
 
 
 # %%
+
+# %% plot AGM
+
+
+def picklesToPlot(label, folder = 'AGM1', max_pic=50, pic_folder = None, show_pic = False):
+    '''
+    Plot pickle
+    only feasible in 2D 
+
+    label      : label array of the 
+    folder     : folder in the result, default 'AGM1'
+    max_pic    : how many iterations to be plotted, default 50
+    pic_folder : if None then use the 'folder' name
+    show_pic   : True it will print on console
+
+    '''
+
+    if pic_folder is not None:
+        pass
+    else:
+        pic_folder = folder
+    
+    pic_path = str(os.getcwd())+ '\\result\\' + pic_folder
+    if not os.path.exists(pic_path):    
+        os.makedirs(pic_path)
+        print("--- Pictures will be saved in : ", pic_path, ' --- ')
+
+    ### color dict ###
+    def generate_color(kind):
+        color_ls = ['r','g','b','#1e1e1e','y']
+        for i in range(kind):
+            color_ls.extend(["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])])
+        return color_ls 
+
+    categories = list(set(label.reshape(-1,).tolist()))
+    colors_ls  = generate_color(len(categories))[:len(categories)]
+    # color - categories mapping
+    color_dict = dict(zip(categories, colors_ls))
+    
+    c = pd.Series(label.reshape(-1,)).apply(lambda x: color_dict[x])
+
+    #### pickle read ####
+    filels = os.listdir(str(os.getcwd()) + '\\result\\' + folder)
+    array_ls = []
+
+    I = 0
+
+    for file in filels[:max_pic]:
+        file = file.split('.')[0]
+        df = pickle_read(file, folder = folder) # read pickles
+        array_ls.append(df)
+        # Plotting 
+        pd.DataFrame(arr).plot.scatter(x=0, y=1, c=c)
+        plt.savefig(pic_path+'\\'+file+'.png')
+        if not show_pic:
+            print(I, 'th-----------------------')
+            plt.show()
+            I += 1
+    
+    return array_ls
+
+
 def my_custom_logger(logger_name, level=logging.INFO):
     """
     Method to return a custom logger with the given name and level
@@ -327,64 +389,3 @@ if __name__ == "__main__":
     x_k = AGM(a.shape[0], lam, delta, X, a, True, tol, logname='weighted_AGM_delta1e-1_lam1e-1_tol1_segment1000', result_fold = 'weighted_AGM_delta1e-1_lam1e-1_tol1_segment1000')
     print('time consuming: ', time.time() - t1)
     
-
-
-# %% plot AGM
-
-
-def picklesToPlot(label, folder = 'AGM1', max_pic=50, pic_folder = None, show_pic = False):
-    '''
-    Plot pickle
-    only feasible in 2D 
-
-    label      : label array of the 
-    folder     : folder in the result, default 'AGM1'
-    max_pic    : how many iterations to be plotted, default 50
-    pic_folder : if None then use the 'folder' name
-    show_pic   : True it will print on console
-
-    '''
-
-    if pic_folder is not None:
-        pass
-    else:
-        pic_folder = folder
-    
-    pic_path = str(os.getcwd())+ '\\result\\' + pic_folder
-    if not os.path.exists(pic_path):    
-        os.makedirs(pic_path)
-        print("--- Pictures will be saved in : ", pic_path, ' --- ')
-
-    ### color dict ###
-    def generate_color(kind):
-        color_ls = ['r','g','b','#1e1e1e','y']
-        for i in range(kind):
-            color_ls.extend(["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])])
-        return color_ls 
-
-    categories = list(set(label.reshape(-1,).tolist()))
-    colors_ls  = generate_color(len(categories))[:len(categories)]
-    # color - categories mapping
-    color_dict = dict(zip(categories, colors_ls))
-    
-    c = pd.Series(label.reshape(-1,)).apply(lambda x: color_dict[x])
-
-    #### pickle read ####
-    filels = os.listdir(str(os.getcwd()) + '\\result\\' + folder)
-    array_ls = []
-
-    I = 0
-
-    for file in filels[:max_pic]:
-        file = file.split('.')[0]
-        df = pickle_read(file, folder = folder) # read pickles
-        array_ls.append(df)
-        # Plotting 
-        pd.DataFrame(arr).plot.scatter(x=0, y=1, c=c)
-        plt.savefig(pic_path+'\\'+file+'.png')
-        if not show_pic:
-            print(I, 'th-----------------------')
-            plt.show()
-            I += 1
-    
-    return array_ls
